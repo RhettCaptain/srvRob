ef4d7de0ca835a6991b00c706e2af1a7a211e1b4
RhettCaptain <425237277@qq.com>
2017-09-16 23:16:24 +0800

    0916

--- pkg_pose/src/init_pose.cpp
+++ pkg_pose/src/init_pose.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "std_msgs/String.h"
 #include "tinyxml2.h"
 #include <cstring>
 #include <unistd.h>
+#include "tf/tf.h"
 
 using namespace tinyxml2;
 
@@ -15,7 +17,7 @@ bool loadInitPose(geometry_msgs::PoseWithCovarianceStamped& initPose,char* xmlPa
 	XMLElement* root = doc.RootElement();
 	initPose.pose.pose.position.x = atof(root->Attribute("x"));
 	initPose.pose.pose.position.y = atof(root->Attribute("y"));
-	initPose.pose.pose.orientation.z = atof(root->Attribute("th"));
+	initPose.pose.pose.orientation = tf::createQuaternionMsgFromYaw(atof(root->Attribute("th")));
 	doc.SaveFile(xmlPath);
 	return true;
 }
@@ -23,20 +25,24 @@ bool loadInitPose(geometry_msgs::PoseWithCovarianceStamped& initPose,char* xmlPa
 int main(int argc,char** argv){
 	ros::init(argc,argv,"node_init_pose");
 	ros::NodeHandle nHandle;
-	ros::Publisher initPosePub = nHandle.advertise<geometry_msgs::PoseWithCovarianceStamped>("topic_init_pose",10);
-
-	int keepTime = 50;
+	ros::Publisher initPosePub = nHandle.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose",10);
+	ros::Publisher resetPub = nHandle.advertise<std_msgs::String>("syscommand",1);
 	geometry_msgs::PoseWithCovarianceStamped initPoseMsg;
+	std_msgs::String resetMsg;
+	initPoseMsg.header.frame_id = "map";
 	char* xmlPath = new char[100];
 	strcpy(xmlPath,"/home/");
 	strcat(xmlPath, getlogin());
 	strcat(xmlPath, "/srv_rob_conf/initPose.xml");
-	while(!loadInitPose(initPoseMsg,xmlPath)){
+	while(ros::ok() && !loadInitPose(initPoseMsg,xmlPath)){
 		ROS_INFO("LOAD FAIL");
 	}
-	ros::Rate wait(10);
-	for(int i=0;i<keepTime;i++){
-		initPosePub.publish(initPoseMsg);
+	resetMsg.data = "reset";
+	int delayTime = 3;
+	ros::Rate wait(1);
+	for(int i=0;i<delayTime;i++){
 		wait.sleep();
 	} 
+	initPosePub.publish(initPoseMsg);
+	resetPub.publish(resetMsg);
 }
--- pkg_shell/init.sh
+++ pkg_shell/init.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 mkdir ~/srv_rob_conf
+#SRC_PATH=$(cd `dirname $0`;cd ..; pwd)
 CONF_PATH=~/srv_rob_conf
-cp initPose.xml $CONF_PATH
+cp initPose.xml ${CONF_PATH}
