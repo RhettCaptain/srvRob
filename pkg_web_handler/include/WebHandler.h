3bfa52ea624dd6bf3d31576018dfcd70cce202ed
rhettcaptain <425237277@qq.com>
2017-10-11 22:55:03 +0800

    1011

--- pkg_web_handler/include/WebHandler.h
+++ pkg_web_handler/include/WebHandler.h
@@ -7,19 +7,25 @@
 #include <string>
 #include <cstring>
 #include <tf/tf.h>
+#include <sstream>
 
 class WebHandler{
 private:
 	SocketClient sock;
 	ros::NodeHandle nHandle;
 	ros::Publisher goalPub;
+	ros::Subscriber poseSub;
+	geometry_msgs::PoseStamped robotPose;
 
 public:
 	WebHandler(const char* ip="192.168.1.250",unsigned short int port=9999);
 
 private:
+	void onSubPose(const geometry_msgs::PoseStamped::ConstPtr& msg);
 	void pubGoal();
 	void motion();
+	void updateMap();
+	void getPose();
 };
 
 
--- pkg_web_handler/src/WebHandler.cpp
+++ pkg_web_handler/src/WebHandler.cpp
@@ -4,11 +4,16 @@ using std::string;
 
 WebHandler::WebHandler(const char* ip,unsigned short int port){
 	goalPub = nHandle.advertise<geometry_msgs::PoseStamped>("topic_goal",10);
+	poseSub = nHandle.subscribe("topic_robot_pose",10,&WebHandler::onSubPose,this);
+	robotPose.pose.position.x = 0;
+	robotPose.pose.position.y = 0;
+	robotPose.pose.orientation = tf::createQuaternionMsgFromYaw(0);
 	while(ros::ok() &&  !sock.connectServer(ip,port) ){
 	//	connecting...
 	}
 std::cout << "connected" << std::endl;
 	while(ros::ok()){
+		ros::spinOnce();
 		string cmd;
 		if(sock.readline(cmd)>0){
 			if(cmd == "CMD_PUB_GOAL"){
@@ -17,11 +22,21 @@ std::cout << "connected" << std::endl;
 			else if(cmd == "CMD_MOTION"){
 				motion();
 			}
+			else if(cmd == "CMD_UPDATE_MAP"){
+				updateMap();
+			}
+			else if(cmd == "CMD_GET_POSE"){
+std::cout << "get pose " << std::endl;
+				getPose();
+			}
 		}
 		
 	}
 }
 
+void WebHandler::onSubPose(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	robotPose = *msg;
+}
 void WebHandler::pubGoal(){
 	char* content = new char[30];
 	char split = ';';
@@ -48,4 +63,25 @@ void WebHandler::motion(){
 	string content;
 	sock.readline(content);
 std::cout << content << std::endl;
+}
+
+void WebHandler::updateMap(){
+	char* mapPath = new char[100];
+	strcpy(mapPath,"/home/");
+	strcat(mapPath,getlogin());
+	strcat(mapPath,"/srv_rob_conf/map.xml");
+//string test;
+//sock.readSock(test);
+	sock.recFile(mapPath);
+std::cout << "ok" << std::endl;
+}
+
+void WebHandler::getPose(){
+	string strPose;
+	stringstream ss;
+	ss << robotPose.pose.position.x << ";" << robotPose.pose.position.y << ";" << tf::getYaw(robotPose.pose.orientation) << ";";
+	ss >> strPose;
+	strPose += "\n";
+std::cout << strPose << std::endl;
+	sock.writeSock(strPose);
 }
