138c32bf84e708ef4d3adb57dcfe5cc909676e22
rhettcaptain <425237277@qq.com>
2018-01-23 16:18:17 +0800

    joystick

--- pkg_joystick/src/main.cpp
+++ pkg_joystick/src/main.cpp
@@ -3,7 +3,8 @@
 int main(int argc,char** argv){
 	ros::init(argc,argv,"node_joystick");
 //	Joystick* stick = new Joystick(0,6,1,7,6,7);	//ps3
-	Joystick* stick = new Joystick(0,4,1,5,4,5);	//betop
+//	Joystick* stick = new Joystick(0,4,1,5,4,5);	//betop
+	Joystick* stick = new Joystick(0,4,1,5,5,4);	//gjw betop
 	stick->setSpd(0.25,0.3,0.33,0.3,0.4,0.9);
 	stick->setPubRate(50);
 	stick->setEnable(false);
